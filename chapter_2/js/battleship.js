b196b5beebd2ac2ea96ac3c2b5c9df83d5e51212
Anna Kozyreva <annakozyreva@MacBook-Pro-Anna.local>
2018-12-29 17:40:40 +0100

    first commit

---
+++ chapter_2/js/battleship.js
@@ -0,0 +1,35 @@
+console.log('test');
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+console.log(location1, location2, location3);
+var guess;
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+while (!isSunk){
+	guess = prompt("Ready, aim fire! Enter a number 0-6");
+	if (guess < 0 || guess >6){
+		alert("Please enter a vaild cell number");
+	}
+	else{
+		guesses += 1;
+		if (guess == location1 || guess == location2 || guess == location3) {
+			alert("HIT!");
+			hits += 1;
+			if (hits == 3) {
+				isSunk = true;
+				alert('You sank my battleship!');
+				}
+			}
+		else{
+			alert("MISS");
+			}
+
+		}
+}
+var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses);
+alert(stats);
